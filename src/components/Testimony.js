47e56329e035e2310bee250d994496dcddd34c83
Steven <stevensvg59@gmail.com>
2024-01-03 10:36:02 -0500

    Proyect 1 - React practice

---
+++ src/components/Testimony.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../stylesheets/Testimony.css';
+
+function Testimony(props){
+return(
+	<div className='container-testimony'>
+		<img 
+			className='image-testimony'
+			src={require(`../images/${props.image}.png`)}
+			alt='{props.image} photography' />
+		<div className='container-text-testimony'>
+			<p className='name-testimony'>
+				<strong>{props.name}</strong> in {props.country}
+			</p>
+			<p className='job-title-testimony'>
+				{props.job} at <strong>{props.company}</strong>
+			</p>
+			<p className='text-testimony'>"{props.testimony}"</p>
+		</div>
+	</div>
+	);
+}
+
+export default Testimony;
